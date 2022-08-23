8ba28a822845f8f12115f226ee430c5562b7b92a
Mateo Evangelista <evangelista2002@gmail.com>
2022-08-23 20:45:28 -0300

    primera modificacion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+//NavBar
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Pet-Shop "Timmy"</h1>
+      <h2>Mateo Evangelista</h2>
+    </div>
+  );
+}
